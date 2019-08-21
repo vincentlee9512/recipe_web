476e5e7b2a080059d497e5a3f155a9855dd06026
vincentlee9512 <li000053@stthomas.edu>
2019-08-21 15:21:18 +0800

    created app 'blogs' and complete db settings

--- recipe_web/settings.py
+++ recipe_web/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'blogs.apps.BlogsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'recipe_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'recipe_web',
+        'HOST': '127.0.0.1',
+        'POST': 3306,
+        'USER': 'vincent',
+        'PASSWORD': '123456',
     }
 }
 
