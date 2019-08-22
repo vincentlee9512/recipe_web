b9a84724243d5074b56f7600c2f93eaddb32364a
vincentlee9512 <li000053@stthomas.edu>
2019-08-22 10:48:42 +0800

    可以注册新的用户

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.models import User
+from django.contrib import messages, auth
 
 def login(request):
     return render(request, 'accounts/login.html')
@@ -10,6 +11,37 @@ def logout(request):
 
 
 def register(request):
+    if request.method == 'POST':
+        print("Get the POST")
+
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        print(f'username: {username}, email: {email}, password: {password}, password2: {password2}')
+
+        # 检测两个密码是否相同
+        if password == password2:
+            # 检测这个用户名是否被占用
+            if User.objects.filter(username=username).exists():
+                messages.error(request, '此用户名已被注册')
+                redirect('register')
+            else:
+                # 检测这个 email 是否已被注册
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, '此 email 已被使用')
+                    redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, email=email, password=password)
+
+                    user.save()
+                    messages.success(request, '你已经成功注册账号，请登陆')
+                    return redirect('login')
+        else:
+            messages.error(request, '两次输入的密码不匹配')
+            return redirect('register')
+
     return render(request, 'accounts/register.html')
 
 
